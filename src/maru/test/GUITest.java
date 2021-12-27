3958f075f2011bf8fbc3a3aa9b310f8817217e5a
國丸裕太 <yu.kuni.maru@gmail.com>
2021-12-27 17:32:22 +0900

    ディレクトリ中のファイルを取得して表示する方法を把握

--- src/maru/test/GUITest.java
+++ src/maru/test/GUITest.java
@@ -1,26 +1,61 @@
 package maru.test;
 import javafx.application.*;
+import javafx.event.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.stage.*;
 import javafx.scene.layout.*;
+import java.util.*;
+import java.io.*;
+
 /**/
 public class GUITest extends Application{
 	Button btn;
+	ArrayList<Image> figureList;
+	Image[] figures;
+	ImageView view;
+	Iterator<Image> iterator;
 	public static void main(String[] args) {
 		launch(args);
 	}
 	@Override
 	public void start(Stage stage) throws Exception {
-		Scene scene;
-		BorderPane bp = new BorderPane();
-		btn = new Button("計測開始");
-		bp.setCenter(btn);
-		scene = new Scene(bp);
-		stage.setScene(scene);
-		stage.setTitle("テスト");
-		stage.setWidth(640);
-		stage.setHeight(480);
-		stage.show();
+		try {
+			Scene scene;
+			BorderPane bp = new BorderPane();
+			view = new ImageView();
+			view.setFitHeight(360);
+			view.setFitWidth(500);
+			figureList = new ArrayList<Image>();
+			Arrays.asList(new File("img/").listFiles()).stream()
+			.map(file -> {
+				try {
+					return new FileInputStream(file);
+				} catch (FileNotFoundException e1) {
+					return null;
+				}
+			})
+			.map(stream -> new Image(stream))
+			.forEach(image -> figureList.add(image));
+			iterator = figureList.iterator();		
+			btn = new Button("切り替え");
+			btn.setOnAction(e -> nextImage(e));
+			bp.setCenter(btn);
+			bp.setBottom(view);
+			scene = new Scene(bp);
+			stage.setScene(scene);
+			stage.setTitle("テスト");
+			stage.setWidth(640);
+			stage.setHeight(480);
+			stage.show();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private void nextImage(ActionEvent e) {
+		if(iterator.hasNext()) {
+			view.setImage(iterator.next());
+		}
 	}
 }
